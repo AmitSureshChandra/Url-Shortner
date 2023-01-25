612676dd16a3ab68032cbe8898e526a4593368cd
ak00029 <akumar00029@gmail.com>
2023-01-25 14:09:36 +0530

    issue#16 - test utils added

--- src/test/kotlin/com/github/amitsureshchandra/urlshortner/UrlShortnerApplicationTests.kt
+++ src/test/kotlin/com/github/amitsureshchandra/urlshortner/UrlShortnerApplicationTests.kt
@@ -1,11 +1,21 @@
 package com.github.amitsureshchandra.urlshortner
 
+import com.github.amitsureshchandra.urlshortner.controller.UrlController
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+
 
 @SpringBootTest
-class UrlShortnerApplicationTests {
-//	@Test
-//	fun contextLoads() {
-//	}
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class UrlShortnerApplicationTests(@Autowired var urlController: UrlController) {
+	@Test
+	fun contextLoads() {
+		assertNotNull(urlController)
+	}
 }
