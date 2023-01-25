954ce6e0537c18daf2897c1451e2fabdfccfde00
ak00029 <akumar00029@gmail.com>
2023-01-25 14:09:36 +0530

    issue #16 - test utils added

--- src/main/kotlin/com/github/amitsureshchandra/urlshortner/config/BeanConfig.kt
+++ src/main/kotlin/com/github/amitsureshchandra/urlshortner/config/BeanConfig.kt
@@ -1,5 +1,6 @@
 package com.github.amitsureshchandra.urlshortner.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
@@ -10,5 +11,10 @@ class BeanConfig {
     @Bean
     fun restTemplate(): RestTemplate{
         return RestTemplate();
+    }
+
+    @Bean
+    fun objectMapper(): ObjectMapper{
+        return ObjectMapper();
     }
 }
